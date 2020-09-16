598f7f8c3d1b6c9cd0497634ac8a906818826ce4
Hilal <hilal.guvenc@segmentify.com>
2020-09-16 22:53:54 +0300

    added country and temperature

--- index.js
+++ index.js
@@ -2,6 +2,11 @@
   "https://fcc-weather-api.freecodecamp.repl.co/api/current?lat=35&lon=139"
 )
   .then((response) => response.json())
-  .then((data) => document.getElementById("card-container").href = data.weather[0].icon);
+  .then((data) => {
+    document.getElementById("icon").src = data.weather[0].icon;
+    document.getElementById("country").innerText = data.sys.country;
+    document.getElementById("temp").innerText = data.main.temp;
+  }) 
+  
  
    
