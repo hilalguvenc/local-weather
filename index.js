9d46a13201fc56e4ed0bfd22cd52befdd82cf144
Hilal <hilal.guvenc@segmentify.com>
2020-09-20 14:07:45 +0300

    edited css, created new p tags, fetched new weather information data

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-   fetch(
-  "https://fcc-weather-api.freecodecamp.repl.co/api/current?lat=35&lon=139"
-)
+fetch("https://fcc-weather-api.freecodecamp.repl.co/api/current?lat=35&lon=139")
   .then((response) => response.json())
   .then((data) => {
     document.getElementById("icon").src = data.weather[0].icon;
-    document.getElementById("country").innerText = data.sys.country;
+    document.getElementById("country").innerText = data.name;
     document.getElementById("temp").innerText = data.main.temp + "°";
-  }) 
-  
- 
-   
+    document.getElementById("weather-text").innerText = data.weather[0].main;
+    document.getElementById("pressure").innerText =
+      "Pressure" + data.main.pressure;
+    document.getElementById("humidity").innerText =
+      "Humidity" + data.main.humidity;
+    document.getElementById("visibility").innerText =
+      "Visibility" + data.visibility;
+    document.getElementById("wind").innerText = "Wind" + data.wind.speed;
+  });
