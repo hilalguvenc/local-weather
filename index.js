1f8bfbd9d08866eaecdad3663fb9d60949626b1c
Hilal <hilal.guvenc@segmentify.com>
2020-09-20 17:16:36 +0300

    added media query

--- index.js
+++ index.js
@@ -5,11 +5,15 @@ fetch("https://fcc-weather-api.freecodecamp.repl.co/api/current?lat=35&lon=139")
     document.getElementById("country").innerText = data.name;
     document.getElementById("temp").innerText = data.main.temp + "°";
     document.getElementById("weather-text").innerText = data.weather[0].main;
-    document.getElementById("pressure").innerText =
-      "Pressure "  + data.main.pressure + "hPa";
-    document.getElementById("humidity").innerText =
-      "Humidity " + data.main.humidity  + "%";
-    document.getElementById("visibility").innerText =
-      "Visibility " + data.visibility + "km";
-    document.getElementById("wind").innerText = "Wind " + data.wind.speed + "km/h";
+    document.getElementById("pressure-text").innerText = "Pressure ";
+    document.getElementById("pressure-data").innerText =
+      data.main.pressure + "hPa";
+    document.getElementById("humidity-text").innerText = "Humidity ";
+    document.getElementById("humidity-data").innerText =
+      data.main.humidity + "%";
+    document.getElementById("visibility-text").innerText = "Visibility ";
+    document.getElementById("visibility-data").innerText =
+      data.visibility + "km";
+    document.getElementById("wind-text").innerText = "Wind ";
+    document.getElementById("wind-data").innerText = data.wind.speed + "km/h";
   });
